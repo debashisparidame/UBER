f026ed6b4f2e2abf82631467edf1ed71e91a31ee
Debashis Parida <dpdebashisparida@gmail.com>
2025-02-05 17:00:34 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const app = require('./app');
+const { initializeSocket } = require('./socket');
+const port = process.env.PORT || 7070;
+
+const server = http.createServer(app);
+
+initializeSocket(server);
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
